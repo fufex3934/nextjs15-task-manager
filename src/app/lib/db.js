caa64a45ebf9e4b05b2b860c40e6de1b0b751713
fufex3934 <fufawakgari174@gmail.com>
2025-04-18 14:48:34 +0300

    create db connecion

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,2 +1,18 @@
 import mongoose from "mongoose";
 
+const MONGO_URI = process.env.MONGO_URI;
+let isConnected = false;
+
+export const connectToDatabase = async () => {
+  if (isConnected) return;
+  try {
+    const db = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = !!db.connection[0].readyState;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Mongo Connection failed",error.message);
+  }
+};
